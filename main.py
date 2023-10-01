ae7deffcd55285a75c0fbdb8bbbe4f7de8b4de82
PerhapsStudios <122779229+PerhapsStudios@users.noreply.github.com>
2023-10-01 00:20:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all(), case_insensitive=True)
+bot = commands.Bot(command_prefix="/", intents=discord.Intents.all(), case_insensitive=True)
 
 @bot.event
 async def on_ready():
